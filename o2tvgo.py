ae03abfa22d815abe799705dff4a595400f67558
eldendiss <denis.voltmann@gmail.com>
2021-06-29 02:27:32 +0200

    Change app version in common header This avoids application.incompatible-version error

--- o2tvgo.py
+++ o2tvgo.py
@@ -11,7 +11,7 @@ __license__ = "MIT"
 __version__ = "1.1.8"
 __email__ = "stepanort@gmail.com"
 
-_COMMON_HEADERS = {"X-NanguTv-App-Version": "Android#1.2.9",
+_COMMON_HEADERS = {"X-NanguTv-App-Version": "Android#7.6.3",
                    "X-NanguTv-Device-Name": "Nexus 7",
                    "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.1.1; Nexus 6 Build/LMY47A)",
                    "Accept-Encoding": "gzip",
